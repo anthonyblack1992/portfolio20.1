8f4d940f912ee24e2c05663816870684fc20e19b
Anthony Black <anthonyblack1992@gmail.com>
2024-11-06 12:40:52 -0600

    changed file path

--- src/main.jsx
+++ src/main.jsx
@@ -8,10 +8,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import AboutMe from "./pages/aboutme.jsx";
-import Contact from "./pages/contact.jsx";
-import Portfolio from "./pages/portfolio.jsx";
-import Resume from "./pages/resume.jsx";
+import AboutMe from "./pages/AboutMe.jsx";
+import Contact from "./pages/Contact.jsx";
+import Portfolio from "./pages/Portfolio.jsx";
+import Resume from "./pages/Resume.jsx";
 
 
 
